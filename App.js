e5d556dd21bce1f5ba2b3be8a96b18ad92c8f095
Harsh Mehra <me.harshmehra07@gmail.com>
2024-07-14 21:52:18 +0530

    update

--- App.js
+++ App.js
@@ -89,6 +89,7 @@ const Body = () => {
             </div>
 
             <div className="restaurant-container">
+
                 <RestCardComponent 
                     image="https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_264,h_288,c_fill/RX_THUMBNAIL/IMAGES/VENDOR/2024/6/27/fe6afdda-d264-438a-80d4-78837536a36e_44507.jpg"
                     resName="Kanha" cuisine="North Indian, Sweets" stars="4.2 stars" time="20 min" 
@@ -97,6 +98,11 @@ const Body = () => {
                 <RestCardComponent
                     image="https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_264,h_288,c_fill/xemfgnlxuwzskxr0h3nt"
                     resName="Sahu Chai" cuisine="Chai, Fast Fooad" stars="4.0 stars" time="30 min"
+                /> 
+                      
+                <RestCardComponent
+                    image="https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/d0450ce1a6ba19ea60cd724471ed54a8"
+                    resName="Domino's" cuisine="Pizza's, Pastas, Desserts" stars="4.3 stars" time="25 min"
                 />       
                      
             </div>
