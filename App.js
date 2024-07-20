d64091f801b8dffc0e43be06f3666e9964695490
Harsh Mehra <me.harshmehra07@gmail.com>
2024-07-20 11:46:37 +0530

    update

--- App.js
+++ App.js
@@ -172,6 +172,358 @@ const restList = [
                         "iconType": "ICON_TYPE_EMPTY"
                       }
             }          
+},
+
+{
+    "info": {
+        "id": "56623",
+        "name": "Anandji Sweets",
+        "cloudinaryImageId": "64d8ec906c52b7d62aaa47cd38bab13a",
+        "locality": "Sector 1",
+        "areaName": "Malviya Nagar",
+        "costForTwo": "₹200 for two",
+        "cuisines": [
+          "Sweets",
+          "Desserts",
+          "Snacks",
+          "Indian"
+        ],
+        "avgRating": 4.5,
+        "veg": true,
+        "parentId": "32920",
+        "avgRatingString": "4.5",
+        "totalRatingsString": "10K+",
+        "sla": {
+          "deliveryTime": 24,
+          "lastMileTravel": 3,
+          "serviceability": "SERVICEABLE",
+          "slaString": "20-25 mins",
+          "lastMileTravelString": "3.0 km",
+          "iconType": "ICON_TYPE_EMPTY"
+        },
+        "availability": {
+          "nextCloseTime": "2024-07-16 22:00:00",
+          "opened": true
+        },
+        "badges": {
+          "imageBadges": [
+            {
+              "imageId": "Rxawards/_CATEGORY-Mithai.png",
+              "description": "Delivery!"
+            },
+            {
+              "imageId": "Rxawards/_CATEGORY-Snacks.png",
+              "description": "Delivery!"
+            },
+            {
+              "imageId": "v1695133679/badges/Pure_Veg111.png",
+              "description": "pureveg"
+            }
+          ]
+        },
+        "isOpen": true,
+        "type": "F",
+        "badgesV2": {
+          "entityBadges": {
+            "textBased": {
+              
+            },
+            "imageBased": {
+              "badgeObject": [
+                {
+                  "attributes": {
+                    "imageId": "Rxawards/_CATEGORY-Mithai.png",
+                    "description": "Delivery!"
+                  }
+                },
+                {
+                  "attributes": {
+                    "imageId": "Rxawards/_CATEGORY-Snacks.png",
+                    "description": "Delivery!"
+                  }
+                },
+                {
+                  "attributes": {
+                    "description": "pureveg",
+                    "imageId": "v1695133679/badges/Pure_Veg111.png"
+                  }
+                }
+              ]
+            },
+            "textExtendedBadges": {
+              
+            }
+          }
+        },
+            
+            }
+},
+{
+    "info": {
+                  "id": "729690",
+                  "name": "GudMishri",
+                  "cloudinaryImageId": "20c6e89e00a2268a01d057e64ee1ca5c",
+                  "locality": "University Marg",
+                  "areaName": "Lal Kothi",
+                  "costForTwo": "₹500 for two",
+                  "cuisines": [
+                    "South Indian"
+                  ],
+                  "avgRating": 4.5,
+                  "veg": true,
+                  "parentId": "281854",
+                  "avgRatingString": "4.5",
+                  "totalRatingsString": "500+",
+                  "sla": {
+                    "deliveryTime": 26,
+                    "lastMileTravel": 3.7,
+                    "serviceability": "SERVICEABLE",
+                    "slaString": "25-30 mins",
+                    "lastMileTravelString": "3.7 km",
+                    "iconType": "ICON_TYPE_EMPTY"
+                  },
+                  "availability": {
+                    "nextCloseTime": "2024-07-16 22:15:00",
+                    "opened": true
+                  },
+                  "badges": {
+                    "imageBadges": [
+                      {
+                        "imageId": "v1695133679/badges/Pure_Veg111.png",
+                        "description": "pureveg"
+                      }
+                    ]
+                  },
+                  "isOpen": true,
+                  "type": "F",
+                  "badgesV2": {
+                    "entityBadges": {
+                      "textBased": {
+                        
+                      },
+                      "imageBased": {
+                        "badgeObject": [
+                          {
+                            "attributes": {
+                              "imageId": "v1695133679/badges/Pure_Veg111.png",
+                              "description": "pureveg"
+                            }
+                          }
+                        ]
+                      },
+                      "textExtendedBadges": {
+                        
+                      }
+                    }
+                  },
+            }
+},
+{
+    "info": {
+                  "id": "791987",
+                  "name": "Burgrill - The Win Win Burger",
+                  "cloudinaryImageId": "f596863116da4b4688b48e813e25000d",
+                  "locality": "Raja Park",
+                  "areaName": "Raja Park",
+                  "costForTwo": "₹200 for two",
+                  "cuisines": [
+                    "Snacks"
+                  ],
+                  "avgRating": 4.3,
+                  "parentId": "302366",
+                  "avgRatingString": "4.3",
+                  "totalRatingsString": "100+",
+                  "promoted": true,
+                  "adTrackingId": "cid=14912357~p=2~adgrpid=14912357#ag1~mp=SWIGGY_IN~bl=FOOD~aet=RESTAURANT~aeid=791987~eid=6a6453c5-15c4-47fc-a6b0-26e24c559d21~srvts=1721126006231~collid=45995",
+                  "sla": {
+                    "deliveryTime": 27,
+                    "lastMileTravel": 4.3,
+                    "serviceability": "SERVICEABLE",
+                    "slaString": "25-30 mins",
+                    "lastMileTravelString": "4.3 km",
+                    "iconType": "ICON_TYPE_EMPTY"
+                  },
+                  "availability": {
+                    "nextCloseTime": "2024-07-17 04:00:00",
+                    "opened": true
+                  },
+                  "badges": {
+                    
+                  },
+                  "isOpen": true,
+                  "type": "F",
+                  "badgesV2": {
+                    "entityBadges": {
+                      "textBased": {
+                        
+                      },
+                      "imageBased": {
+                        
+                      },
+                      "textExtendedBadges": {
+                        
+                      }
+                    }
+                  },
+                  "aggregatedDiscountInfoV3": {
+                    "header": "₹125 OFF",
+                    "subHeader": "ABOVE ₹249",
+                    "discountTag": "FLAT DEAL"
+                  },
+            }
+},
+{
+    "info": {
+                  "id": "45605",
+                  "name": "Domino's Pizza",
+                  "cloudinaryImageId": "d0450ce1a6ba19ea60cd724471ed54a8",
+                  "locality": "Malviya Nagar",
+                  "areaName": "Malviya Nagar",
+                  "costForTwo": "₹400 for two",
+                  "cuisines": [
+                    "Pizzas",
+                    "Italian",
+                    "Pastas",
+                    "Desserts"
+                  ],
+                  "avgRating": 4.2,
+                  "parentId": "2456",
+                  "avgRatingString": "4.2",
+                  "totalRatingsString": "10K+",
+                  "sla": {
+                    "deliveryTime": 25,
+                    "lastMileTravel": 4.2,
+                    "serviceability": "SERVICEABLE",
+                    "slaString": "20-25 mins",
+                    "lastMileTravelString": "4.2 km",
+                    "iconType": "ICON_TYPE_EMPTY"
+                  },
+                  "availability": {
+                    "nextCloseTime": "2024-07-17 02:59:00",
+                    "opened": true
+                  },
+                    
+            }
+},
+{
+"info": {
+                  "id": "743253",
+                  "name": "JBT- Jaipur Burger Truck",
+                  "cloudinaryImageId": "fa4cafa64c73bc8f9530bf89f347b306",
+                  "locality": "janpath road",
+                  "areaName": "C Scheme",
+                  "costForTwo": "₹300 for two",
+                  "cuisines": [
+                    "Burgers",
+                    "Pizzas",
+                    "Beverages",
+                    "Snacks"
+                  ],
+                  "avgRating": 4.4,
+                  "parentId": "13739",
+                  "avgRatingString": "4.4",
+                  "totalRatingsString": "500+",
+                  "sla": {
+                    "deliveryTime": 27,
+                    "lastMileTravel": 3.6,
+                    "serviceability": "SERVICEABLE",
+                    "slaString": "25-30 mins",
+                    "lastMileTravelString": "3.6 km",
+                    "iconType": "ICON_TYPE_EMPTY"
+                  },
+                  "availability": {
+                    "nextCloseTime": "2024-07-17 03:00:00",
+                    "opened": true
+                  },
+                  "badges": {
+                    
+                  },
+                  "isOpen": true,
+                  "type": "F",
+                  "badgesV2": {
+                    "entityBadges": {
+                      "textBased": {
+                        
+                      },
+                      "imageBased": {
+                        
+                      },
+                    },
+                  }
+        }
+},
+{
+"info": {
+                  "id": "61900",
+                  "name": "Chhabra's Pure Veg",
+                  "cloudinaryImageId": "szjeg7uilf8s4npggvg9",
+                  "locality": "Valmiki Colony",
+                  "areaName": "Jagatpura",
+                  "costForTwo": "₹199 for two",
+                  "cuisines": [
+                    "North Indian",
+                    "Thalis",
+                    "Indian",
+                    "Rajasthani",
+                    "Biryani",
+                    "Punjabi",
+                    "Beverages",
+                    "Tandoor",
+                    "Snacks"
+                  ],
+                  "avgRating": 4.5,
+                  "veg": true,
+                  "parentId": "230551",
+                  "avgRatingString": "4.5",
+                  "totalRatingsString": "10K+",
+                  "sla": {
+                    "deliveryTime": 25,
+                    "lastMileTravel": 2.9,
+                    "serviceability": "SERVICEABLE",
+                    "slaString": "25-30 mins",
+                    "lastMileTravelString": "2.9 km",
+                    "iconType": "ICON_TYPE_EMPTY"
+                  },
+                  "availability": {
+                    "nextCloseTime": "2024-07-16 23:00:00",
+                    "opened": true
+                  },
+                  "badges": {
+                    "imageBadges": [
+                      {
+                        "imageId": "v1695133679/badges/Pure_Veg111.png",
+                        "description": "pureveg"
+                      }
+                    ]
+                  },
+                  "isOpen": true,
+                  "type": "F",
+                  "badgesV2": {
+                    "entityBadges": {
+                      "textBased": {
+                        
+                      },
+                      "imageBased": {
+                        "badgeObject": [
+                          {
+                            "attributes": {
+                              "imageId": "v1695133679/badges/Pure_Veg111.png",
+                              "description": "pureveg"
+                            }
+                          }
+                        ]
+                      },
+                      "textExtendedBadges": {
+                        
+                      }
+                    }
+                  },
+                  "aggregatedDiscountInfoV3": {
+                    "header": "₹125 OFF",
+                    "subHeader": "ABOVE ₹249",
+                    "discountTag": "FLAT DEAL"
+                  },
+        },
 }
 ]
 
