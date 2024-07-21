2559584dc4c26b2564c4e104cfeea8e9a3bf8471
Harsh Mehra <me.harshmehra07@gmail.com>
2024-07-21 18:21:33 +0530

    updta

---
+++ README.md
@@ -0,0 +1,21 @@
+## Blueprint of our app
+/*
+ * Major 3 components we'll have:
+ * 🔸Header Component
+ *      -- Logo
+ *      -- Nav items (Home AboutUs Cart)
+ *  🔸Body Component
+ *      -- Search Bar with search button
+ *      -- Restaurant Container
+ *          --  Individual Restaurant Cards
+ *              🔸Img of restaurant
+ *              🔸Name of restaurant
+ *              🔸Star rating
+ *              🔸Cuisines served by restaurant
+ * 🔸Footer Component
+ *      -- Copyright
+ *      -- Links
+ *      -- Address
+ *      -- Contact Us
+ * 
+ */
--- src/App.js
+++ src/App.js
@@ -1,23 +1,4 @@
-/**
- * Major 3 components we'll have:
- * 🔸Header Component
- *      -- Logo
- *      -- Nav items (Home AboutUs Cart)
- * 🔸Body Component
- *      -- Search Bar with search button
- *      -- Restaurant Container
- *          --  Individual Restaurant Cards
- *              🔸Img of restaurant
- *              🔸Name of restaurant
- *              🔸Star rating
- *              🔸Cuisines served by restaurant
- * 🔸Footer Component
- *      -- Copyright
- *      -- Links
- *      -- Address
- *      -- Contact Us
- * 
- */
+
 import React from "react";
 import ReactDOM from "react-dom/client";
 
